4feb77eecf9a4d6c8ac186476337468faab4578f
Anuj3558 <127491755+Anuj3558@users.noreply.github.com>
2023-06-17 17:15:02 +0530

    Add files via upload

---
+++ odd even lets upgrade.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number you want to check");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("Number is even.");
+	}
+	else
+	printf("Number is odd.")
+}
